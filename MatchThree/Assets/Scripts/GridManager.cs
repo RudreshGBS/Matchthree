7d5e638d7e740edefb01307282856654219169ef
Rudresh <gbs.rudresh@gmail.com>
2022-01-22 17:01:31 +0530

    Basic Game

--- MatchThree/Assets/Scripts/GridManager.cs
+++ MatchThree/Assets/Scripts/GridManager.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections;
+﻿using rudscreation.Utils;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GridManager : MonoBehaviour
+public class GridManager : Singleton<GridManager>
 {
     public List<Sprite> Sprites = new List<Sprite>();
     public GameObject TilePrefab;
@@ -19,6 +19,7 @@ public class GridManager : MonoBehaviour
     /// <summary>
     /// Init grid
     /// </summary>
+
     void InitGrid()
     {
         Vector3 positionOffset = transform.position - new Vector3(GridDimension * Distance / 2.0f, GridDimension * Distance / 2.0f, 0); 
@@ -27,17 +28,143 @@ public class GridManager : MonoBehaviour
             {
                 GameObject newTile = Instantiate(TilePrefab); 
                 SpriteRenderer renderer = newTile.GetComponent<SpriteRenderer>(); 
-                renderer.sprite = Sprites[Random.Range(0, Sprites.Count)]; 
+                renderer.sprite = Sprites[Random.Range(0, Sprites.Count)];
+                Tile tile = newTile.AddComponent<Tile>();
+                tile.Position = new Vector2Int(column, row);
                 newTile.transform.parent = transform; 
                 newTile.transform.position = new Vector3(column * Distance, row * Distance, 0) + positionOffset; 
+                Grid[column, row] = newTile;            }
+    }
+    /// <summary>
+    /// this function will swap the the tiles 
+    /// </summary>
+    /// <param name="tile1Position"></param>
+    /// <param name="tile2Position"></param>
+    public void SwapTiles(Vector2Int tile1Position, Vector2Int tile2Position)
+    {
 
-                Grid[column, row] = newTile; 
-            }
+       
+        GameObject tile1 = Grid[tile1Position.x, tile1Position.y];
+        SpriteRenderer renderer1 = tile1.GetComponent<SpriteRenderer>();
+
+        GameObject tile2 = Grid[tile2Position.x, tile2Position.y];
+        SpriteRenderer renderer2 = tile2.GetComponent<SpriteRenderer>();
+
+        Sprite temp = renderer1.sprite;
+        renderer1.sprite = renderer2.sprite;
+        renderer2.sprite = temp;
+
+        bool changesOccurs = CheckMatches();
+        if (!changesOccurs)
+        {
+            temp = renderer1.sprite;
+            renderer1.sprite = renderer2.sprite;
+            renderer2.sprite = temp;
+        }
+        else
+        {
+            do
+            {
+                FillHoles();
+            } while (CheckMatches());
+        }
+    }
+    /// <summary>
+    /// this will retrn sprit rander 
+    /// </summary>
+    /// <param name="column"></param>
+    /// <param name="row"></param>
+    /// <returns></returns>
+    SpriteRenderer GetSpriteRendererAt(int column, int row)
+    {
+        if (column < 0 || column >= GridDimension
+             || row < 0 || row >= GridDimension)
+            return null;
+        GameObject tile = Grid[column, row];
+        SpriteRenderer renderer = tile.GetComponent<SpriteRenderer>();
+        return renderer;
     }
 
-    // Update is called once per frame
-    void Update()
+    bool CheckMatches()
     {
-        
+        HashSet<SpriteRenderer> matchedTiles = new System.Collections.Generic.HashSet<SpriteRenderer>();
+        for (int row = 0; row < GridDimension; row++)
+        {
+            for (int column = 0; column < GridDimension; column++)
+            {
+                SpriteRenderer current = GetSpriteRendererAt(column, row);
+
+                List<SpriteRenderer> horizontalMatches = FindColumnMatchForTile(column, row, current.sprite);
+                if (horizontalMatches.Count >= 2)
+                {
+                    matchedTiles.UnionWith(horizontalMatches);
+                    matchedTiles.Add(current); 
+                }
+
+                List<SpriteRenderer> verticalMatches = FindRowMatchForTile(column, row, current.sprite);
+                if (verticalMatches.Count >= 2)
+                {
+                    matchedTiles.UnionWith(verticalMatches);
+                    matchedTiles.Add(current);
+                }
+            }
+        }
+
+        foreach (SpriteRenderer renderer in matchedTiles)
+        {
+            renderer.sprite = null;
+        }
+        return matchedTiles.Count > 0;
+    }
+
+    private List<SpriteRenderer> FindRowMatchForTile(int column, int row, Sprite sprite)
+    {
+        List<SpriteRenderer> result = new List<SpriteRenderer>();
+        for (int i = row + 1; i < GridDimension; i++)
+        {
+            SpriteRenderer nextRow = GetSpriteRendererAt(column, i);
+            if (nextRow.sprite != sprite)
+            {
+                break;
+            }
+            result.Add(nextRow);
+        }
+        return result;
+    }
+
+    private List<SpriteRenderer> FindColumnMatchForTile(int column, int row, Sprite sprite)
+    {
+        List<SpriteRenderer> result = new List<SpriteRenderer>();
+        for (int i = column + 1; i < GridDimension; i++)
+        {
+            SpriteRenderer nextColumn = GetSpriteRendererAt(i, row);
+            if (nextColumn.sprite != sprite)
+            {
+                break;
+            }
+            result.Add(nextColumn);
+        }
+        return result;
+    }
+
+    void FillHoles()
+    {
+        for (int column = 0; column < GridDimension; column++)
+        {
+            for (int row = 0; row < GridDimension; row++)
+            {
+                while (GetSpriteRendererAt(column, row).sprite == null)
+                {
+                    for (int filler = row; filler < GridDimension - 1; filler++)
+                    {
+                        SpriteRenderer current = GetSpriteRendererAt(column, filler); 
+                        SpriteRenderer next = GetSpriteRendererAt(column, filler + 1);
+                        current.sprite = next.sprite;
+                    }
+                    SpriteRenderer last = GetSpriteRendererAt(column, GridDimension - 1);
+                    last.sprite = Sprites[Random.Range(0, Sprites.Count)];
+                }
+            }
+        }
     }
 }
---
+++ MatchThree/Assets/Scripts/Tile.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+
+    private static Tile selected;
+    private SpriteRenderer tileRenderer;
+    public Vector2Int Position;
+
+    private void Start() 
+    {
+        tileRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void Select() 
+    {
+        tileRenderer.color = Color.grey;
+    }
+
+    public void Unselect() 
+    {
+        tileRenderer.color = Color.white;
+    }
+
+    private void OnMouseDown() 
+    {
+        if (selected != null)
+        {
+            if (selected == this)
+                return;
+            selected.Unselect();
+            if (Vector2Int.Distance(selected.Position, Position) == 1)
+            {
+                GridManager.Instance.SwapTiles(Position, selected.Position);
+                selected = null;
+            }
+            else
+            {
+                selected = this;
+                Select();
+            }
+        }
+        else
+        {
+            selected = this;
+            Select();
+        }
+    }
+   
+}

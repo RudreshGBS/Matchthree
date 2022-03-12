9fda4d6feae40031000f6f591596ba9c26bdc4a5
Karan <karan1ugd43wp@gmail.com>
2022-03-12 10:46:31 +0530

    Level UI Integrated;

--- MatchThree/Assets/Scripts/LevelData.cs
+++ MatchThree/Assets/Scripts/LevelData.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelData : MonoBehaviour
 {
@@ -9,10 +11,15 @@ public class LevelData : MonoBehaviour
     public Color Enabled;
     public Color Disabled;
     public Transform RocketEndPosition;
-    private SpriteRenderer planet;
+    private Image planet;
+    [SerializeField]
+    private Button planetButton;
+    [SerializeField]
+    private TMP_Text planetButtonText;
     private void Start()
     {
-        planet = GetComponent<SpriteRenderer>();
+        planet = GetComponent<Image>();
+        planetButtonText.text = Level.ToString();
         SetupLevel();
     }
     public void SetupLevel()
@@ -20,10 +27,16 @@ public class LevelData : MonoBehaviour
         if(!isActiveLevel)
         {
             planet.color = Disabled;
+            planetButton.image.color = Disabled;
+            planetButton.interactable = false;
+            planetButtonText.color = Disabled;
         }
         else
         {
             planet.color = Enabled;
+            planetButton.image.color = Enabled;
+            planetButton.interactable = true;
+            planetButtonText.color = Enabled;
         }
     }
 
